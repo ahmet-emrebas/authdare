4005c7c819bd7b24d6d8ac88fbeb7ae711c495df
ahmet-emrebas <none>
2021-07-16 02:36:20 -0500

    initial..

--- ngnest/libs/angular/sidenav/src/lib/sidenav.component.spec.ts
+++ ngnest/libs/angular/sidenav/src/lib/sidenav.component.spec.ts
@@ -8,9 +8,8 @@ describe('SidenavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidenavComponent ]
-    })
-    .compileComponents();
+      declarations: [SidenavComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,13 @@ describe('SidenavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render *', () => {
+    const fixture = TestBed.createComponent(SidenavComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const ref = compiled.querySelector('*');
+    expect(ref).not.toBeNull();
   });
 });
