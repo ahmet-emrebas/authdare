7435a77eca6b99fa51683c222e34fd82511c8791
ahmet-emrebas <none>
2021-07-25 11:17:52 -0500

    models init

---
+++ authdare-server/src/models/base/base.entity.ts
@@ -0,0 +1,9 @@
+import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class BaseEntity<T>{
+    @PrimaryGeneratedColumn() id: number;
+    @CreateDateColumn() created_at: Date;
+    @UpdateDateColumn() updated_at: Date;
+    @DeleteDateColumn() deleted_at: Date
+    constructor(obj?: Partial<T>) { Object.assign(this, obj) }
+}
