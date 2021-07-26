fe59a88e4a156360aca291a4b2e754f55eb9970b
ahmet-emrebas <none>
2021-07-26 12:16:02 -0500

    added user and org model

---
+++ authdare-server/libs/models/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./base";
+export * from "./org";
+export * from "./user";
---
+++ authdare-server/libs/models/src/user/user.entity.ts
@@ -0,0 +1,15 @@
+import { Org } from './../org';
+import { BaseEntity } from './../base/';
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+
+
+@Entity()
+export class User extends BaseEntity<User>{
+    @Column() email: string;
+    @Column() password: string;
+    @Column() role: string;
+
+    @ManyToOne(() => Org, org => org.id, { eager: false, createForeignKeyConstraints: true })
+    @JoinColumn()
+    org: Org
+}
