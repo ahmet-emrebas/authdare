7a0458b042b5f6f2fd45e196e741126e0d2c7e50
ahmet-emrebas <none>
2021-07-28 10:23:48 -0500

    ...

--- authdare-server/apps/authdare-server/src/main.ts
+++ authdare-server/apps/authdare-server/src/main.ts
@@ -45,8 +45,8 @@ async function bootstrap() {
   app.use(favicon(join(__dirname, '..', '..', '..', 'client', 'favicon.ico')));
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Authdare Api')
+    .setDescription('Atomic role and permission assignment ... ')
     .setVersion('1.0')
     .build();
 
